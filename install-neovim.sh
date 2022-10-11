2d38a517b3ebec95176cb26ce2240c865ad70c0a
Abdulkerim Ekrem Bekir <aekrembekir@gmail.com>
2022-10-12 00:41:41 +0300

    Update install-neovim.sh

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y python3 python2.7 ruby ruby-dev wget python3-pip python3-venv nodejs npm neovim
+sudo apt install -y python3 python2.7 ruby ruby-dev wget python3-pip python3-venv nodejs npm neovim curl cpanminus
 
 python3 -m pip install virtualenv
 python3 -m virtualenv -p python2.7 ~/nvim-venvs/py2.7-virtualenv
@@ -19,6 +19,8 @@ sudo gem install neovim
 sudo gem environment
 
 sudo npm install -g neovim
+
+sudo cpanm -n Neovim::Ext
 
 wget https://github.com/Aerbil/neovim-configuration/raw/main/init.vim
 mkdir -p ~/.config/nvim/
